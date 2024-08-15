1052b7f5430a7750cb0dd1d6c3e9cbb265ba0c75
Trenton <17258002+trentonsnyder@users.noreply.github.com>
2024-08-15 16:02:11 -0600

    trying node 18

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/**
+ * @type {import('next').NextConfig}
+ * */
+const nextConfig = {
+  output: 'standalone',
+}
 
-export default nextConfig;
+module.exports = nextConfig
