c3fb8936b6d60dac27dd77dcd9a0fb72e49ad28a
Trenton <17258002+trentonsnyder@users.noreply.github.com>
2024-08-15 14:50:06 -0600

    adding @ prop route to layout

--- src/app/[trainer]/(base)/layout.tsx
+++ src/app/[trainer]/(base)/layout.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren } from "react";
 
-function BaseLayout({children, params}: PropsWithChildren<{ params: { trainer: string }}>) {
+function BaseLayout({children, params, dialog}: PropsWithChildren<{ params: { trainer: string }, dialog: React.ReactNode}>) {
   return <div>{children}</div>
 }
 
